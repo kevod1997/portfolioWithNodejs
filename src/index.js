11652dd997aa617ce4e160be3b9d44bc023792d1
Kevin Defalco <kevindefalco@gmail.com>
2022-10-31 17:57:11 -0300

    puerto autorizado

--- src/index.js
+++ src/index.js
@@ -14,5 +14,4 @@ import indexRoutes from "./routes/index.js";
 
  app.use(express.static(join(__dirname, 'public')))
  
- app.listen(3000)
- console.log('server is listening on port', 3000);
+ app.listen(process.env.PORT || 3000)
